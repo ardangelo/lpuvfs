d7efdec530d0ae6ea65fdef761a33fadcd2e4fb4
Andrew D'Angelo <dangeloandrew@outlook.com>
2018-01-16 21:16:56 -0600

    stub out functions for directory support

--- int.c
+++ int.c
@@ -9,15 +9,29 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+/* directory control */
+#include <sys/types.h>
+#include <dirent.h>
+
 #include "vfs.h"
 
-/* fd functions */
+/* file function types */
 typedef int (*orig_open_t)(const char *pathname, int flags, ...);
 typedef int (*orig_creat_t)(const char *pathname, mode_t mode);
 typedef ssize_t (*orig_read_t)(int fd, void *buf, size_t count);
 typedef ssize_t (*orig_write_t)(int fd, const void *buf, size_t count);
 typedef int (*orig_unlink_t)(const char *pathname);
 typedef int (*orig_close_t)(int fd);
+
+/* directory function types */
+typedef struct dirent* (*orig_readdir_t)(DIR *dirp);
+typedef DIR* (*orig_opendir_t)(const char *name);
+typedef DIR* (*orig_fdopendir_t)(int fd);
+
+/* file info types */
+typedef int (*orig_stat_t)(const char *pathname, struct stat *statbuf);
+
+/* fd functions */
 
 int open(const char *pathname, int flags, ...) {
 	if (should_fake(pathname)) {
@@ -26,6 +40,9 @@ int open(const char *pathname, int flags, ...) {
 
 	orig_open_t orig_open = (orig_open_t)dlsym(RTLD_NEXT, "open");
 	return orig_open(pathname, flags);
+}
+int open64(const char *pathname, int flags, ...) {
+	return open(pathname, flags);
 }
 
 int creat(const char *pathname, mode_t mode) {
@@ -78,4 +95,38 @@ int fclose(FILE *fp) {
 
 	orig_fclose_t orig_fclose = (orig_fclose_t)dlsym(RTLD_NEXT, "fclose");
 	return orig_fclose(fp);
+}
+
+
+/* directory functions */
+struct dirent* readdir(DIR *dirp) {
+	fprintf(stderr, "caught readdir\n");
+
+	orig_readdir_t orig_readdir = (orig_readdir_t)dlsym(RTLD_NEXT, "readdir");
+	return orig_readdir(dirp);
+}
+
+DIR* opendir(const char *name) {
+	fprintf(stderr, "caught opendir\n");
+
+	orig_opendir_t orig_opendir = (orig_opendir_t)dlsym(RTLD_NEXT, "opendir");
+	return orig_opendir(name);
+}
+
+DIR* fdopendir(int fd) {
+	fprintf(stderr, "caught fdopendir\n");
+
+	orig_fdopendir_t orig_fdopendir = (orig_fdopendir_t)dlsym(RTLD_NEXT, "fdopendir");
+	return orig_fdopendir(fd);
+}
+
+/* file info functions */
+int stat(const char *pathname, struct stat *statbuf) {
+	fprintf(stderr, "caught stat\n");
+
+	orig_stat_t orig_stat = (orig_stat_t)dlsym(RTLD_NEXT, "stat");
+	return orig_stat(pathname, statbuf);
+}
+int stat64(const char *pathname, struct stat *statbuf) {
+	return stat(pathname, statbuf);
 }
